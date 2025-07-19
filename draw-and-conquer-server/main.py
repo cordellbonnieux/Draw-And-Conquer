3722e6438cf538023b78e9ce9c5457d0648709b5
Catalpa <caojingzifang@gmail.com>
2025-07-18 22:00:49 -0700

    Server initial commit

---
+++ draw-and-conquer-server/main.py
@@ -0,0 +1,58 @@
+import json
+import threading
+import time
+import argparse
+
+from server import TCPServer, UDPServer
+
+
+def request_handler(text: str) -> str:
+    try:
+        message = json.loads(text)
+    except json.JSONDecodeError:
+        message = {"error": "Invalid JSON format"}
+
+    return json.dumps(message)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--host",
+        type=str,
+        required=True,
+        help="Host to bind",
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        required=True,
+        help="Port number",
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    host = args.host
+    port = args.port
+
+    tcp = TCPServer(host, port, request_handler)
+    tcp_thread = threading.Thread(target=tcp.start, daemon=True)
+    tcp_thread.start()
+
+    udp = UDPServer(host, port, request_handler)
+    udp_thread = threading.Thread(target=udp.start, daemon=True)
+    udp_thread.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ draw-and-conquer-server/server.py
@@ -0,0 +1,102 @@
+import socket
+import threading
+import uuid
+from typing import Callable
+
+
+class TCPServer:
+    def __init__(
+        self,
+        host: str,
+        port: int,
+        request_handler: Callable[[str], str],
+    ):
+        self.host = host
+        self.port = port
+        self.request_handler = request_handler
+
+        self.clients = {}
+        self.lock = threading.Lock()
+
+    def _get_client_id(self) -> str:
+        return str(uuid.uuid4())
+
+    def _handle_connection(
+        self,
+        sock: socket.socket,
+        addr: tuple[str, int],
+    ) -> None:
+        try:
+            with self.lock:
+                client_id = self._get_client_id()
+                self.clients[client_id] = (sock, addr)
+
+            with sock:
+                while True:
+                    data = sock.recv(65535)
+                    if not data:
+                        break
+
+                    response = self.request_handler(data.decode("utf-8"))
+                    sock.sendall(response.encode("utf-8"))
+        finally:
+            with self.lock:
+                del self.clients[client_id]
+
+    def start(self) -> None:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((self.host, self.port))
+        sock.listen(128)
+
+        try:
+            while True:
+                conn, addr = sock.accept()
+                thread = threading.Thread(
+                    target=self._handle_connection,
+                    args=(conn, addr),
+                )
+                thread.daemon = True
+                thread.start()
+        finally:
+            sock.close()
+
+
+class UDPServer:
+    def __init__(
+        self,
+        host: str,
+        port: int,
+        request_handler: Callable[[str], str],
+    ):
+        self.host = host
+        self.port = port
+        self.request_handler = request_handler
+
+        self.lock = threading.Lock()
+
+    def _handle_datagram(
+        self,
+        sock: socket.socket,
+        data: bytes,
+        addr: tuple[str, int],
+    ) -> None:
+        response = self.request_handler(data.decode("utf-8"))
+        with self.lock:
+            sock.sendto(response.encode("utf-8"), addr)
+
+    def start(self) -> None:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((self.host, self.port))
+
+        try:
+            while True:
+                data, addr = sock.recvfrom(65535)
+                thread = threading.Thread(
+                    target=self._handle_datagram, args=(sock, data, addr)
+                )
+                thread.daemon = True
+                thread.start()
+        finally:
+            sock.close()
